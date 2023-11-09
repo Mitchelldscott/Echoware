08aca0faef2124d88a1b701a40b121b385f12f58
Mitchell Scott <misc4432@colorado.edu>
2023-11-08 17:34:00 -0700

    clean up

--- install/install.bash
+++ install/install.bash
@@ -9,7 +9,7 @@ echo -e "\033[1;33m[INFO]\033[1;30m Installing Echoware"
 
 if [[ ! -f env/bin/activate ]]; then
 	echo -e "\033[1;33m[INFO]\033[1;30m Initializing virtual environment"
-	python3 -m venv env .
+	python3 -m venv env
 fi
 
 source env/bin/activate
